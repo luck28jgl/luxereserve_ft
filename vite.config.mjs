2cbb2a3d5506c014d712b6181241c4455d9ab68f
JGL <luckibarra15@gmail.com>
2025-04-18 20:30:33 -0600

    config-hgpages

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
+import ghpages from 'vite-plugin-gh-pages'
 
 export default defineConfig({
   plugins: [
     vue(),
-    tailwindcss()
+    tailwindcss(),
+    ghpages()
+
   ],
+  base: '/luxereserve_ft/',
 });
